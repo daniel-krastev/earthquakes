2c65437d2599ee1dadec0d8c7a7a5d242d747ab5
Daniel Krastev <danielkrastev@yahoo.com>
2019-01-27 01:10:07 +0000

    git ignore file and code reformating

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,100 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-const String SIGNIFICANT = "significant";
-const String ALL = "all";
-const String _4_5 = "4.5";
-const String _2_5 = "2.5";
-const String _1_0 = "1.0";
-const String HOUR = "hour";
-const String DAY = "day";
-const String WEEK = "week";
-const String MONTH = "month";
-
-const String PERIOD_KEY = "Period.key";
-const String SIGNIFICANCE_KEY = "Significance.key";
-
-const String BASE_URL =
-    "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/SIGNIFICANCE_PERIOD.geojson";
-
-const int PERIOD_DEFAULT_IDX = 2;
-const int SIGNIFICANCE_DEFAULT_IDX = 1;
-
-SharedPreferences prefs;
-DateFormat _dateFormat = DateFormat("MMMM dd, yyyy h:mm a");
-NumberFormat _partNumberFormat = NumberFormat("#0.##");
-NumberFormat _latNumberFormat = NumberFormat("00.##");
-NumberFormat _longNumberFormat = NumberFormat("000.##");
 
 main() async {
   prefs = await SharedPreferences.getInstance();
   runApp(MaterialApp(title: "Earthqakes App", home: Home()));
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
+const String ALL = "all";
+const String BASE_URL =
+    "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/SIGNIFICANCE_PERIOD.geojson";
+const String DAY = "day";
+const String HOUR = "hour";
+const String MONTH = "month";
+const int PERIOD_DEFAULT_IDX = 2;
+const String PERIOD_KEY = "Period.key";
+const int SIGNIFICANCE_DEFAULT_IDX = 1;
+
+const String SIGNIFICANCE_KEY = "Significance.key";
+const String SIGNIFICANT = "significant";
+
+const String WEEK = "week";
+
+const String _1_0 = "1.0";
+const String _2_5 = "2.5";
+
+const String _4_5 = "4.5";
+SharedPreferences prefs;
+DateFormat _dateFormat = DateFormat("MMMM dd, yyyy h:mm a");
+NumberFormat _latNumberFormat = NumberFormat("00.##");
+NumberFormat _longNumberFormat = NumberFormat("000.##");
+
+NumberFormat _partNumberFormat = NumberFormat("#0.##");
+
+Future<Map> getJson(String url) async {
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
+}
+
+String normalizeDistance(num rawKm) {
+  double rawKmAbs = rawKm.abs().toDouble();
+  return "${_partNumberFormat.format(rawKmAbs)} km";
+}
+
+String normalizeLat(num rawLat) {
+  double rawLatAbs = rawLat.abs().toDouble();
+  if (rawLatAbs <= 90.0) {
+    StringBuffer res = StringBuffer();
+    double seconds;
+    double decimalPart = rawLatAbs - rawLatAbs.truncateToDouble();
+
+    //degree
+    res.write(_latNumberFormat.format(rawLatAbs.truncate()));
+    res.write("\u00b0 ");
+    //minutes
+    res.write(_partNumberFormat.format((decimalPart * 60).truncate()));
+    res.write("\' ");
+    //seconds
+    seconds = ((decimalPart * 60) - (decimalPart * 60).truncateToDouble()) * 60;
+    res.write(_partNumberFormat.format(seconds));
+    res.write("\" ");
+    //north or south
+    res.write(rawLat.sign == -1.0 ? "S " : (rawLat.sign == 1.0 ? "N " : ""));
+
+    return res.toString();
+  }
+  return "UNKNOWN";
+}
+
+String normalizeLong(num rawLong) {
+  double rawLongAbs = rawLong.abs().toDouble();
+  if (rawLongAbs <= 180.0) {
+    StringBuffer res = StringBuffer();
+    double seconds;
+    double decimalPart = rawLongAbs - rawLongAbs.truncateToDouble();
+
+    //degree
+    res.write(_longNumberFormat.format(rawLongAbs.truncate()));
+    res.write("\u00b0 ");
+    //minutes
+    res.write(_partNumberFormat.format((decimalPart * 60).truncate()));
+    res.write("\' ");
+    //seconds
+    seconds = ((decimalPart * 60) - (decimalPart * 60).truncateToDouble()) * 60;
+    res.write(_partNumberFormat.format(seconds));
+    res.write("\" ");
+    //east or west
+    res.write(rawLong.sign == -1.0 ? "W " : (rawLong.sign == 1.0 ? "E " : ""));
+
+    return res.toString();
+  }
+  return "UNKNOWN";
 }
 
 class EntryData {
@@ -45,6 +102,11 @@ class EntryData {
   final String title;
 
   EntryData(this.value, this.title);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
@@ -70,57 +132,13 @@ class _HomeState extends State<Home> {
   bool _isLoaded;
 
   _HomeState() {
-    _currentSign = _significanceList[prefs.getInt(SIGNIFICANCE_KEY) ?? SIGNIFICANCE_DEFAULT_IDX].value;
-    _currentPeriod = _periodList[prefs.get(PERIOD_KEY) ?? PERIOD_DEFAULT_IDX].value;
+    _currentSign = _significanceList[
+            prefs.getInt(SIGNIFICANCE_KEY) ?? SIGNIFICANCE_DEFAULT_IDX]
+        .value;
+    _currentPeriod =
+        _periodList[prefs.get(PERIOD_KEY) ?? PERIOD_DEFAULT_IDX].value;
     _currentURL = _getURL();
     _init();
-  }
-
-  void _init() {
-    _isLoaded = false;
-    _futureData = getJson(_currentURL);
-    _futureData.then((s) {
-      setState(() {
-        _data = s['features'];
-        _isLoaded = true;
-      });
-    });
-  }
-
-  void _onSignChanged(final EntryData data) {
-    final String newSign = data.value;
-    if (newSign == _currentSign) return;
-    _currentSign = newSign;
-    prefs.setInt(SIGNIFICANCE_KEY, _significanceList.indexOf(data));
-    _onURLChanged(_getURL());
-  }
-
-  void _onPeriodChanged(final EntryData data) {
-    final String newPeriod = data.value;
-    if (newPeriod == _currentPeriod) return;
-    _currentPeriod = newPeriod;
-    prefs.setInt(PERIOD_KEY, _periodList.indexOf(data));
-    _onURLChanged(_getURL());
-  }
-
-  void _onURLChanged(final String newURL) {
-    setState(() {
-      _currentURL = newURL;
-      _isLoaded = false;
-      _futureData = getJson(newURL);
-      _futureData.then((s) {
-        setState(() {
-          _data = s['features'];
-          _isLoaded = true;
-        });
-      });
-    });
-  }
-
-  String _getURL() {
-    return BASE_URL
-        .replaceAll(RegExp('SIGNIFICANCE'), _currentSign)
-        .replaceAll(RegExp('PERIOD'), _currentPeriod);
   }
 
   @override
@@ -234,6 +252,83 @@ class _HomeState extends State<Home> {
     );
   }
 
+  String _getURL() {
+    return BASE_URL
+        .replaceAll(RegExp('SIGNIFICANCE'), _currentSign)
+        .replaceAll(RegExp('PERIOD'), _currentPeriod);
+  }
+
+  void _init() {
+    _isLoaded = false;
+    _futureData = getJson(_currentURL);
+    _futureData.then((s) {
+      setState(() {
+        _data = s['features'];
+        _isLoaded = true;
+      });
+    });
+  }
+
+  void _onPeriodChanged(final EntryData data) {
+    final String newPeriod = data.value;
+    if (newPeriod == _currentPeriod) return;
+    _currentPeriod = newPeriod;
+    prefs.setInt(PERIOD_KEY, _periodList.indexOf(data));
+    _onURLChanged(_getURL());
+  }
+
+  void _onSignChanged(final EntryData data) {
+    final String newSign = data.value;
+    if (newSign == _currentSign) return;
+    _currentSign = newSign;
+    prefs.setInt(SIGNIFICANCE_KEY, _significanceList.indexOf(data));
+    _onURLChanged(_getURL());
+  }
+
+  void _onURLChanged(final String newURL) {
+    setState(() {
+      _currentURL = newURL;
+      _isLoaded = false;
+      _futureData = getJson(newURL);
+      _futureData.then((s) {
+        setState(() {
+          _data = s['features'];
+          _isLoaded = true;
+        });
+      });
+    });
+  }
+
+  void _showDialog(BuildContext ctx, int pos) {
+    showDialog(
+        context: ctx,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text("Position"),
+            content: SingleChildScrollView(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                    "Lat: ${normalizeLat(_data[pos]["geometry"]["coordinates"][1])}"),
+                Text(
+                    "Long: ${normalizeLong(_data[pos]["geometry"]["coordinates"][0])}"),
+                Text(
+                    "Depth: ${normalizeDistance(_data[pos]["geometry"]["coordinates"][2])}")
+              ],
+            )),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("OK"),
+              )
+            ],
+          );
+        });
+  }
+
   void _showLongDialog(ctx, pos) {
     showDialog(
         context: ctx,
@@ -258,94 +353,4 @@ class _HomeState extends State<Home> {
           );
         });
   }
-
-  void _showDialog(BuildContext ctx, int pos) {
-    showDialog(
-        context: ctx,
-        builder: (ctx) {
-          return AlertDialog(
-            title: Text("Position"),
-            content: SingleChildScrollView(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("Lat: ${normalizeLat(
-                        _data[pos]["geometry"]["coordinates"][1])}"),
-                Text("Long: ${normalizeLong(
-                        _data[pos]["geometry"]["coordinates"][0])}"),
-                Text("Depth: ${normalizeDistance(
-                        _data[pos]["geometry"]["coordinates"][2])}")
-              ],
-            )),
-            actions: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("OK"),
-              )
-            ],
-          );
-        });
-  }
-}
-
-String normalizeLat(num rawLat) {
-  double rawLatAbs = rawLat.abs().toDouble();
-  if (rawLatAbs <= 90.0) {
-    StringBuffer res = StringBuffer();
-    double seconds;
-    double decimalPart = rawLatAbs - rawLatAbs.truncateToDouble();
-
-    //degree
-    res.write(_latNumberFormat.format(rawLatAbs.truncate()));
-    res.write("\u00b0 ");
-    //minutes
-    res.write(_partNumberFormat.format((decimalPart * 60).truncate()));
-    res.write("\' ");
-    //seconds
-    seconds = ((decimalPart * 60) - (decimalPart * 60).truncateToDouble()) * 60;
-    res.write(_partNumberFormat.format(seconds));
-    res.write("\" ");
-    //north or south
-    res.write(rawLat.sign == -1.0 ? "S " : (rawLat.sign == 1.0 ? "N " : ""));
-
-    return res.toString();
-  }
-  return "UNKNOWN";
-}
-
-String normalizeLong(num rawLong) {
-  double rawLongAbs = rawLong.abs().toDouble();
-  if (rawLongAbs <= 180.0) {
-    StringBuffer res = StringBuffer();
-    double seconds;
-    double decimalPart = rawLongAbs - rawLongAbs.truncateToDouble();
-
-    //degree
-    res.write(_longNumberFormat.format(rawLongAbs.truncate()));
-    res.write("\u00b0 ");
-    //minutes
-    res.write(_partNumberFormat.format((decimalPart * 60).truncate()));
-    res.write("\' ");
-    //seconds
-    seconds = ((decimalPart * 60) - (decimalPart * 60).truncateToDouble()) * 60;
-    res.write(_partNumberFormat.format(seconds));
-    res.write("\" ");
-    //east or west
-    res.write(rawLong.sign == -1.0 ? "W " : (rawLong.sign == 1.0 ? "E " : ""));
-
-    return res.toString();
-  }
-  return "UNKNOWN";
-}
-
-String normalizeDistance(num rawKm) {
-  double rawKmAbs = rawKm.abs().toDouble();
-  return "${_partNumberFormat.format(rawKmAbs)} km";
-}
-
-Future<Map> getJson(String url) async {
-  http.Response response = await http.get(url);
-  return json.decode(response.body);
 }
